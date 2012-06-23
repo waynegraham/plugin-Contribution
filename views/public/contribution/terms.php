9131b4ccc6f8eba5742fdb1e1a556d248088a690
Wayne Graham <wayne.graham@virginia.edu>
2012-06-23 18:45:07 -0400

    Adds head and foot to terms template
    Terms view was missing head and foot so it would render the text, but outside the context of the theme.

--- views/public/contribution/terms.php
+++ views/public/contribution/terms.php
@@ -1,1 +1,8 @@
-<?php echo get_option('contribution_consent_text'); ?>
+<?php
+    $head = array('title' => 'Contribution Terms of Service',
+              'bodyclass' => 'contribution');
+    head($head);
+
+    echo get_option('contribution_consent_text');
+
+    foot();
